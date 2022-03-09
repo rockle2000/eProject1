dab1d2877ffd0a1ab8e170fe350a3b5a26a78667
rockle2000 <quanghhuuyy000@gmail.com>
2022-03-09 22:00:06 +0700

    add appointment and feedback

--- views/home/contact.php
+++ views/home/contact.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +10,7 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
   <meta name="copyright" content="MACode ID, https://macodeid.com/">
+  <link rel="icon" href="../../assets/img/icons/hospital-building.png">
 
   <title>Contact us</title>
 
@@ -19,6 +23,8 @@
   <link rel="stylesheet" href="../../assets/vendor/animate/animate.css">
 
   <link rel="stylesheet" href="../../assets/css/theme.css">
+  <!-- Toastr -->
+  <link rel="stylesheet" href="../../plugins/toastr/toastr.min.css">
 </head>
 <body>
 
@@ -45,27 +51,72 @@
     <div class="container">
       <h1 class="text-center wow fadeInUp">Add feedback</h1>
 
-      <form class="contact-form mt-5">
+      <form class="contact-form mt-5" action="#" method="POST">
         <div class="row mb-3">
           <div class="col-sm-6 py-2 wow fadeInLeft">
-            <label for="fullName">Name</label>
-            <input type="text" id="fullName" class="form-control" placeholder="Full name..">
+            <label for="txtFullName">Name</label>
+            <input type="text" id="txtFullName" name="txtFullName" class="form-control" placeholder="Full name.." required>
           </div>
           <div class="col-sm-6 py-2 wow fadeInRight">
-            <label for="emailAddress">Email</label>
-            <input type="text" id="emailAddress" class="form-control" placeholder="Email address..">
+            <label for="txtEmail">Email</label>
+            <input type="text" id="txtEmail" name="txtEmail" class="form-control" placeholder="Email address.." required>
           </div>
           <div class="col-12 py-2 wow fadeInUp">
-            <label for="subject">Subject</label>
-            <input type="text" id="subject" class="form-control" placeholder="Enter subject..">
+            <label for="txtSubject">Subject</label>
+            <input type="text" id="txtSubject" name="txtSubject" class="form-control" placeholder="Enter subject.." required>
           </div>
           <div class="col-12 py-2 wow fadeInUp">
-            <label for="message">Message</label>
-            <textarea id="message" class="form-control" rows="8" placeholder="Enter Message.."></textarea>
+            <label for="txtMessage">Message</label>
+            <textarea id="txtMessage" name="txtMessage" class="form-control" rows="8" placeholder="Enter Message.."></textarea>
           </div>
         </div>
-        <button type="submit" class="btn btn-primary wow zoomIn">Send feedback</button>
+        <button type="submit" name="btn_submit" class="btn btn-primary wow zoomIn">Send feedback</button>
       </form>
+      <?php
+      require_once "../../db_connect.php";
+      require_once "../../util.php";
+      if (isset($_POST["btn_submit"])) {
+        $conn = OpenCon();
+        $flag = true;
+        if (isset($_POST["txtFullName"]) && isset($_POST["txtEmail"])  && isset($_POST["txtSubject"])&& isset($_POST["txtMessage"])) {
+          $full_name = test_input($_POST["txtFullName"]);
+          $email = test_input($_POST["txtEmail"]);
+          $subject = test_input($_POST["txtSubject"]);
+          $message = test_input($_POST["txtMessage"]);
+          if (ctype_space($full_name) || trim($full_name) == "") {
+            $_SESSION['failed'] = "Full name cannot be null";
+            $flag = false;
+          }
+          if (ctype_space($subject) || trim($subject) == "") {
+            $_SESSION['failed'] = "Subject cannot be null";
+            $flag = false;
+          }
+          if (ctype_space($message) || trim($message) == "") {
+            $_SESSION['failed'] = "Message cannot be null";
+            $flag = false;
+          }
+          if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $_SESSION['failed'] = "Email is invalid";
+            $flag = false;
+        }
+          if ($flag) {
+            $stmt = $conn->prepare("INSERT INTO `feedbacks` (`name`,`email`,`subject`, `comment`) VALUES (?,?,?,?)");
+            $stmt->bind_param("ssss", $full_name, $email, $subject, $message);
+            $stmt->execute();
+            if ($stmt->affected_rows >= 1) {
+              $_SESSION['success'] = "Add feedback successfully";
+            } else {
+              $_SESSION['failed'] = "Add feedback failed !";
+            }
+            $stmt->close();
+          }
+          CloseCon($conn);
+        }else{
+          echo 'fial';
+        }
+      } 
+      ?>
+
     </div>
   </div>
 
@@ -91,6 +142,35 @@
   </div> <!-- .banner-home -->
 
   <?php include_once "shared/footer.php" ?>
-  
+  <script src="../../plugins/toastr/toastr.min.js"></script>
+  <script type="text/javascript">
+    <?php
+    if (isset($_SESSION['success'])) {
+    ?>
+      toastr.options = {
+        "timeOut": 3000,
+        "progressBar": true
+      }
+      var message = "<?php echo $_SESSION['success'] ?>";
+      toastr.success(message);
+    <?php
+      unset($_SESSION['success']);
+    }
+    ?>
+
+    <?php
+    if (isset($_SESSION['failed'])) {
+    ?>
+      toastr.options = {
+        "timeOut": 3000,
+        "progressBar": true
+      }
+      var message = "<?php echo $_SESSION['failed'] ?>";
+      toastr.error(message);
+    <?php
+      unset($_SESSION['failed']);
+    }
+    ?>
+  </script>
 </body>
 </html>
--- views/home/home.php
+++ views/home/home.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,6 +11,7 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
   <meta name="copyright" content="MACode ID, https://macodeid.com/">
+  <link rel="icon" href="../../assets/img/icons/hospital-building.png">
 
   <title>Cambridge Hospital</title>
 
@@ -20,6 +24,8 @@
   <link rel="stylesheet" href="../../assets/vendor/animate/animate.css">
 
   <link rel="stylesheet" href="../../assets/css/theme.css">
+  <!-- Toastr -->
+  <link rel="stylesheet" href="../../plugins/toastr/toastr.min.css">
 </head>
 
 <body>
@@ -174,7 +180,7 @@
     </div>
   </div>
 
-  <div class="page-section bg-light">
+  <div class="page-section bg-light" id="success_stories">
     <div class="container">
       <h1 class="text-center wow fadeInUp">Achievements</h1>
       <div class="row mt-5">
@@ -262,52 +268,93 @@
   <div class="page-section">
     <div class="container">
       <h1 class="text-center wow fadeInUp">Make an Appointment</h1>
-
-      <form class="main-form">
+      <form class="main-form" action="#" method="POST">
         <div class="row mt-5 ">
           <div class="col-12 col-sm-6 py-2 wow fadeInLeft">
-            <input type="text" class="form-control" placeholder="Full name">
+            <input type="text" class="form-control" placeholder="Full name" id="txtFullName" name="txtFullName" required>
           </div>
           <div class="col-12 col-sm-6 py-2 wow fadeInRight">
-            <input type="text" class="form-control" placeholder="Email address..">
+            <input type="text" class="form-control" id="txtDob" name="txtDob" placeholder="Date of birth" onfocus="(this.type='date')" onblur="(this.type='text')" required>
           </div>
           <div class="col-12 col-sm-6 py-2 wow fadeInLeft" data-wow-delay="300ms">
-            <input type="date" class="form-control">
+            <input type="text" class="form-control" id="txtDate" name="txtDate" placeholder="Appointment date" onfocus="(this.type='date')" onblur="(this.type='text')" required>
           </div>
           <div class="col-12 col-sm-6 py-2 wow fadeInRight" data-wow-delay="300ms">
-            <select name="departement" id="departement" class="custom-select">
-
+            <select name="ddlService" id="ddlService" class="custom-select">
               <?php
-                require_once "../../db_connect.php";
-                $conn = OpenCon();
-                $sql = "SELECT * FROM services";
-                if ($result = mysqli_query($conn, $sql)) {
-                  if (mysqli_num_rows($result) > 0) {
-                    while ($row = mysqli_fetch_array($result)) {
+              require_once "../../db_connect.php";
+              $conn = OpenCon();
+              $sql = "SELECT * FROM services where `status` = 1";
+              if ($result = mysqli_query($conn, $sql)) {
+                if (mysqli_num_rows($result) > 0) {
+                  while ($row = mysqli_fetch_array($result)) {
               ?>
-                      <option value="<?php echo $row["id"]; ?>"><?php echo $row["service_name"] ?></option>
-                  <?php
-                    }
-                    mysqli_free_result($result);
-                  } 
-                } else {
-                  echo "ERROR: Không thể thực thi câu lệnh $sql. " . mysqli_error($conn);
+                    <option value="<?php echo $row["id"]; ?>"><?php echo $row["service_name"] ?></option>
+              <?php
+                  }
+                  mysqli_free_result($result);
                 }
-                // Đóng kết nối
-                CloseCon($conn);
+              } else {
+                echo "ERROR: Không thể thực thi câu lệnh $sql. " . mysqli_error($conn);
+              }
+              // Đóng kết nối
+              CloseCon($conn);
               ?>
             </select>
           </div>
           <div class="col-12 py-2 wow fadeInUp" data-wow-delay="300ms">
-            <input type="text" class="form-control" placeholder="Number..">
+            <input type="text" class="form-control" placeholder="PhoneNumber" id="txtPhone" name="txtPhone" required>
           </div>
           <div class="col-12 py-2 wow fadeInUp" data-wow-delay="300ms">
-            <textarea name="message" id="message" class="form-control" rows="6" placeholder="Enter message.."></textarea>
+            <textarea class="form-control" rows="6" placeholder="Enter message.." id="txtDescription" name="txtDescription"></textarea>
           </div>
         </div>
 
-        <button type="submit" class="btn btn-primary mt-3 wow zoomIn">Submit Request</button>
+        <button type="submit" name="btn_submit" class="btn btn-primary mt-3 wow zoomIn">Send</button>
       </form>
+      <?php
+      require_once "../../db_connect.php";
+      require_once "../../util.php";
+      if (isset($_POST["btn_submit"])) {
+        $conn = OpenCon();
+        $flag = true;
+        if (isset($_POST["txtFullName"]) && isset($_POST["txtPhone"])  && isset($_POST["txtDob"])  && isset($_POST["txtDate"])) {
+          $full_name = test_input($_POST["txtFullName"]);
+          $phone = test_input($_POST["txtPhone"]);
+          $time = strtotime($_POST["txtDob"]);
+          $dob = date('Y-m-d H:i:s', $time);
+          $time2 = strtotime($_POST["txtDate"]);
+          $ap_date = date('Y-m-d H:i:s', $time2);
+          $description = test_input($_POST["txtDescription"]);
+          $service_id = $_POST["ddlService"];
+          if (ctype_space($full_name) || trim($full_name) == "") {
+            $_SESSION['failed'] = "Full name cannot be null";
+            $flag = false;
+          }
+          if (ctype_space($phone) || trim($phone) == "") {
+            $_SESSION['failed'] = "Phone number cannot be null";
+            $flag = false;
+          }
+          if (!preg_match("/^(([+]{0,1}\d{2})|\d?)[\s-]?[0-9]{2}[\s-]?[0-9]{3}[\s-]?[0-9]{4}$/", $phone)) {
+            $_SESSION['failed'] = "Phone is invalid";
+            $flag = false;
+          }
+          if ($flag) {
+            $stmt = $conn->prepare("INSERT INTO `appointments` (`fullname`,`dob`,`phone_number`, `service_id`,`description`,`appointment_date`) VALUES (?,?,?,?,?,?)");
+            $stmt->bind_param("sssiss", $full_name, $dob, $phone, $service_id, $description, $ap_date);
+            $stmt->execute();
+            if ($stmt->affected_rows >= 1) {
+              $_SESSION['success'] = "Book appointment successfully";
+            } else {
+              $_SESSION['failed'] = "Book appointment failed !";
+            }
+            $stmt->close();
+          }
+          CloseCon($conn);
+          // echo ("<script>location.href = '/eProject1/views/home/home.php';</script>");
+        }
+      } 
+      ?>
     </div>
   </div> <!-- .page-section -->
 
@@ -327,9 +374,36 @@
       </div>
     </div>
   </div> <!-- .banner-home -->
-
   <?php include_once "shared/footer.php" ?>
+  <script src="../../plugins/toastr/toastr.min.js"></script>
+  <script type="text/javascript">
+    <?php
+    if (isset($_SESSION['success'])) {
+    ?>
+      toastr.options = {
+        "timeOut": 3000,
+        "progressBar": true
+      }
+      var message = "<?php echo $_SESSION['success'] ?>";
+      toastr.success(message);
+    <?php
+      unset($_SESSION['success']);
+    }
+    ?>
 
+    <?php
+    if (isset($_SESSION['failed'])) {
+    ?>
+      toastr.options = {
+        "timeOut": 3000,
+        "progressBar": true
+      }
+      var message = "<?php echo $_SESSION['failed'] ?>";
+      toastr.error(message);
+    <?php
+      unset($_SESSION['failed']);
+    }
+    ?>
+  </script>
 </body>
-
 </html>
